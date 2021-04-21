8a90de8a2e53810a5adaa7ea71a4b33b386112a0
Tre' Seymour <wayne.seymour@elastic.co>
2021-04-21 17:17:00 -0600

    Playing with ordinal scales

--- src/app.js
+++ src/app.js
@@ -1,13 +1,10 @@
 document.body.innerHTML = `<h1>Hello from: ${d3.version}</h1>`;
 
-const quantizeScale = d3.scaleQuantize()
-  .domain([0, 100])
+const ordinalScale = d3.scaleOrdinal()
+  .domain(['poor', 'good', 'great'])
   .range(["red", "white", "green"]);
 
 
-console.log(quantizeScale(22));
-console.log(quantizeScale(49));
-console.log(quantizeScale(50));
-console.log(quantizeScale(88));
-
-console.log(quantizeScale.invertExtent('white'))
+console.log(ordinalScale('good'));
+console.log(ordinalScale('poor'))
+console.log(ordinalScale('great'))
