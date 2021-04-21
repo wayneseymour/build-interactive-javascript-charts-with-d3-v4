f2ef2bbbf5d28c52753bcfef65b0766e52215aa7
Tre' Seymour <wayne.seymour@elastic.co>
2021-04-21 17:03:17 -0600

    playing with continuous scales with and without clamping

--- src/app.js
+++ src/app.js
@@ -1,11 +1,15 @@
-document.body.innerHTML = `<h1>${d3.version}</h1>`;
+document.body.innerHTML = `<h1>Hello from: ${d3.version}</h1>`;
 
 const testScoresRange = [0, 100];
-const outputRange = [0, 1];
+const outputRange = [0, 600];
 const linearScale = d3.scaleLinear()
   .domain(testScoresRange)
   .range(outputRange)
+  .clamp(true) // Forces the return value to be within the output range
 
+console.log(`\n### linearScale(-20): \n\t${linearScale(-20)}`);
 console.log(`\n### linearScale(0): \n\t${linearScale(0)}`);
 console.log(`\n### linearScale(50): \n\t${linearScale(50)}`);
+console.log(`\n### linearScale(100): \n\t${linearScale(100)}`);
+
 
