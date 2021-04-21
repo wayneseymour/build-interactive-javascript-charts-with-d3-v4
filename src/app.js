237bbdf0826a0cb38c22e9f5e6aa0c6fa4353c99
Tre' Seymour <wayne.seymour@elastic.co>
2021-04-21 17:14:43 -0600

    Playing with quantize scales...cardinality in output range

--- src/app.js
+++ src/app.js
@@ -1,10 +1,13 @@
 document.body.innerHTML = `<h1>Hello from: ${d3.version}</h1>`;
 
-const timeScale = d3.scaleTime()
-  .domain([new Date(2021, 0, 1), new Date()])
-  .range([0, 100]);
-
-console.log(`\n### timeScale(new Date(2021, 4, 15)): \n\t${timeScale(new Date(2021, 4, 15))}`);
-console.log(`\n### timeScale.invert(50): \n\t${timeScale.invert(50)}`);
+const quantizeScale = d3.scaleQuantize()
+  .domain([0, 100])
+  .range(["red", "white", "green"]);
 
 
+console.log(quantizeScale(22));
+console.log(quantizeScale(49));
+console.log(quantizeScale(50));
+console.log(quantizeScale(88));
+
+console.log(quantizeScale.invertExtent('white'))
