973f58a01c59d3a011a83d203a98ada19e8f0d9d
Tre' Seymour <wayne.seymour@elastic.co>
2021-04-21 17:08:25 -0600

    Playing with time scales and finding the 'middle' of a time line

--- src/app.js
+++ src/app.js
@@ -1,15 +1,10 @@
 document.body.innerHTML = `<h1>Hello from: ${d3.version}</h1>`;
 
-const testScoresRange = [0, 100];
-const outputRange = [0, 600];
-const linearScale = d3.scaleLinear()
-  .domain(testScoresRange)
-  .range(outputRange)
-  .clamp(true) // Forces the return value to be within the output range
+const timeScale = d3.scaleTime()
+  .domain([new Date(2021, 0, 1), new Date()])
+  .range([0, 100]);
 
-console.log(`\n### linearScale(-20): \n\t${linearScale(-20)}`);
-console.log(`\n### linearScale(0): \n\t${linearScale(0)}`);
-console.log(`\n### linearScale(50): \n\t${linearScale(50)}`);
-console.log(`\n### linearScale(100): \n\t${linearScale(100)}`);
+console.log(`\n### timeScale(new Date(2021, 4, 15)): \n\t${timeScale(new Date(2021, 4, 15))}`);
+console.log(`\n### timeScale.invert(50): \n\t${timeScale.invert(50)}`);
 
 
